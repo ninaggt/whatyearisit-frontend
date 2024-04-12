90324e5c70c33a664607b4c6a642ceeaf22f0eaa
Your name <Your email>
2024-04-12 11:36:27 +0200

    update date frontend

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
 fetch('https://whatyearisit-backend-red-psi.vercel.app/year')
  .then(response => response.json()).then();
- const ajd = new Date()
- document.querySelector('#year').textContent = ajd.getFullYear()
+ const data = new Date()
+ document.querySelector('#year').textContent = data.getFullYear()
